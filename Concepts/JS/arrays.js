6a540c90770a3172046bfbd3a8f2501f6c40d9c8
somone-codes <someone_mails@protonmail.com>
2021-06-20 17:14:25 +0530

    arrays

---
+++ Concepts/JS/arrays.js
@@ -0,0 +1,12 @@
+var arrays = [10, 20 , 40]
+
+arrays.push(40)
+
+console.log(arrays.toString())
+
+if (arrays.includes(50)){
+    console.log("Array has 50.")
+}else {
+    console.log("Array doesnt have 50.")
+
+}
