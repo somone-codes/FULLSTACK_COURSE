0d6ddb210a06ce68994536af7d5f366f8db9b6b7
somone-codes <someone_mails@protonmail.com>
2021-07-10 14:24:09 +0530

    Section-33 REACT Breaking app into components

---
+++ Section-33/ReactBasics/src/Components/List.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function List() {
+    return <ul>
+        <li>Apple</li>
+        <li>Orange</li>
+        <li>Grape</li>
+    </ul>
+}
+
+export default List;
---
+++ Section-33/ReactBasics/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./Components/App";
+
+const element = (
+    <App/>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
