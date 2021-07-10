440bbe0908a4f9b7983f3ae6814f29821a58cea9
somone-codes <someone_mails@protonmail.com>
2021-07-10 17:28:23 +0530

    Section-33 REACT Keeper app starting point

---
+++ Section-33/keeperApp/src/Components/KeeperApp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+function KeeperApp() {
+    return  <div>
+                <Header/>
+                <Note/>
+                <Footer/>
+            </div>
+}
+
+export default KeeperApp
---
+++ Section-33/keeperApp/src/index.js
@@ -0,0 +1,10 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import KeeperApp from "./Components/KeeperApp";
+
+const element = (
+    <KeeperApp/>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
