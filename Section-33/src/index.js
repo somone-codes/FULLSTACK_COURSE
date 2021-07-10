d4a892df9425eb9542cd64d145f937e602d7e659
somone-codes <someone_mails@protonmail.com>
2021-07-10 13:08:26 +0530

    Section-33 REACT inline style

--- Section-33/src/index.js
+++ Section-33/src/index.js
@@ -5,10 +5,15 @@ const number = 99;
 const fname = "Some";
 const lname = "One";
 const age = 13;
+const inlineStyle = {
+    color: 'white',
+    backgroundColor: 'blue'
+}
+
 const element = (
     <div>
-        <h1 className="heading" contentEditable="true">Hello {fname + " " + lname}</h1>
-        <p>Is your lucky number {number * 2} and mine is {Math.floor(Math.random() * 100)}</p>
+        <h1 className="heading" contentEditable="true" spellCheck="false" autofocus="false">Hello {fname + " " + lname}</h1>
+        <p style={inlineStyle}>Is your lucky number {number * 2} and mine is {Math.floor(Math.random() * 100)}</p>
         <p>Am i {` ${age} old?`}</p>
         <p>screw you!</p>
     </div>
