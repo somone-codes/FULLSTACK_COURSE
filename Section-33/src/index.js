213108cafa975fea6f9ff2226c2c621f7ce1718c
somone-codes <someone_mails@protonmail.com>
2021-07-10 11:44:22 +0530

    Section-33 REACT Heading with list

--- Section-33/src/index.js
+++ Section-33/src/index.js
@@ -1,4 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const element = (
+    <div>
+        <h1>heading</h1>
+        <ul>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+        </ul>
+    </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
