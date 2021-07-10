f6a7c51dc36e969b73c71cbafb202ad41796f93e
somone-codes <someone_mails@protonmail.com>
2021-07-10 11:47:56 +0530

    Section-33 REACT Expression

--- Section-33/src/index.js
+++ Section-33/src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const number = 99;
+const fname = "Some";
+const lname = "One";
+const age = 13;
 const element = (
     <div>
-        <h1>heading</h1>
-        <ul>
-            <li>item 1</li>
-            <li>item 2</li>
-            <li>item 3</li>
-        </ul>
+        <h1>Hello {fname + " " + lname}</h1>
+        <p>Is your lucky number {number * 2} and mine is {Math.floor(Math.random() * 100)}</p>
+        <p>Am i {` ${age} old?`}</p>
+        <p>screw you!</p>
     </div>
 );
 
