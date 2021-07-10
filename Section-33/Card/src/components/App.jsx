2cfb5f282b1a5299aa943744ea3fa814b3668a8f
somone-codes <someone_mails@protonmail.com>
2021-07-10 20:42:15 +0530

    Section-33 REACT using map to loop contacts

--- Section-33/Card/src/components/App.jsx
+++ Section-33/Card/src/components/App.jsx
@@ -2,26 +2,20 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 
+
+function createCard(contact){
+    return <Card
+        name={contact.name}
+        phone={contact.phone}
+        email={contact.email}
+        image={contact.imgURL}/>
+}
+
 function App() {
   return (
     <div>
         <h1 className="heading">My Contacts</h1>
-        <Card
-            name={contacts[0].name}
-            phone={contacts[0].phone}
-            email={contacts[0].email}
-            image={contacts[0].imgURL}/>
-        <Card
-            name={contacts[1].name}
-            phone={contacts[1].phone}
-            email={contacts[1].email}
-            image={contacts[1].imgURL}/>
-        <Card
-            name={contacts[2].name}
-            phone={contacts[2].phone}
-            email={contacts[2].email}
-            image={contacts[2].imgURL}/>
-        
+        { contacts.map(createCard) }
     </div>
   );
 }
