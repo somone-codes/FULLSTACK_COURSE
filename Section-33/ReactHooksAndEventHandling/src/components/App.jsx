86acb8829875160d9ddb98a7f6a844fde604660c
somone-codes <someone_mails@protonmail.com>
2021-07-13 08:42:39 +0530

    Section-33 REACT EventHandling - FORM - CONTROL COMPONENT - display user input once form is submitted

--- Section-33/ReactHooksAndEventHandling/src/components/App.jsx
+++ Section-33/ReactHooksAndEventHandling/src/components/App.jsx
@@ -3,18 +3,26 @@ import React, {useState} from "react";
 function App() {
     let [isMouseOver, changeState] = useState(false)
     let [inputValue, changeInputValue] = useState("")
+    let [name, changeName] = useState("")
 
-  return (
-    <div className="container">
-      <h1>Hello {inputValue}</h1>
-      <input type="text" placeholder="What's your name?" value={inputValue}
-             onChange={event => changeInputValue(event.target.value)}/>
-      <button style={{backgroundColor: isMouseOver ? "black" : "white"}}
-              onMouseOver={()=>changeState(true)}
-              onMouseOut={()=>changeState(false)}>
-          Submit</button>
-    </div>
-  );
+    function formSubmit(event) {
+        changeName(inputValue);
+        event.preventDefault(); // this is to prevent the next step on form submit which is page refresh
+    }
+
+    return (
+        <div className="container">
+            <h1>Hello {name}</h1>
+            <form onSubmit={formSubmit}>
+                <input type="text" placeholder="What's your name?" value={inputValue}
+                       onChange={event => changeInputValue(event.target.value)}/>
+                <button type="submit" style={{backgroundColor: isMouseOver ? "black" : "white"}}
+                        onMouseOver={()=>changeState(true)}
+                        onMouseOut={()=>changeState(false)}>
+                    Submit</button>
+            </form>
+        </div>
+    );
 
 }
 
