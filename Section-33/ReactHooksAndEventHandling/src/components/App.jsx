f66d1a4f7fbffdc22736518e1ef41c9be24ed404
somone-codes <someone_mails@protonmail.com>
2021-07-13 08:40:42 +0530

    Section-33 REACT EventHandling - capture the user input and display input

--- Section-33/ReactHooksAndEventHandling/src/components/App.jsx
+++ Section-33/ReactHooksAndEventHandling/src/components/App.jsx
@@ -2,20 +2,17 @@ import React, {useState} from "react";
 
 function App() {
     let [isMouseOver, changeState] = useState(false)
-    
-    function onMouseOver() {
-        changeState(true)
-    }
-    function onMouseOut() {
-        changeState(false)
-    }
-    
+    let [inputValue, changeInputValue] = useState("")
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button style={{backgroundColor: isMouseOver ? "black" : "white"}} onMouseOver={onMouseOver}
-              onMouseOut={onMouseOut}>Submit</button>
+      <h1>Hello {inputValue}</h1>
+      <input type="text" placeholder="What's your name?" value={inputValue}
+             onChange={event => changeInputValue(event.target.value)}/>
+      <button style={{backgroundColor: isMouseOver ? "black" : "white"}}
+              onMouseOver={()=>changeState(true)}
+              onMouseOut={()=>changeState(false)}>
+          Submit</button>
     </div>
   );
 
