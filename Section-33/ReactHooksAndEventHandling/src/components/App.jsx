2a5d6d28ffae41167ebbf4712260e5a20b0737e8
somone-codes <someone_mails@protonmail.com>
2021-07-14 08:48:15 +0530

    Section-33 REACT EventHandling - FORM - CONTROL COMPONENT - complex state where state is object

--- Section-33/ReactHooksAndEventHandling/src/components/App.jsx
+++ Section-33/ReactHooksAndEventHandling/src/components/App.jsx
@@ -2,21 +2,51 @@ import React, {useState} from "react";
 
 export default function App() {
     let [isMouseOver, changeState] = useState(false)
-    let [inputValue, changeInputValue] = useState("")
-    let [name, changeName] = useState("")
+    let [inputValue, changeInputValue] = useState({
+        fName: "",
+        lName: ""
+    })
+    let [name, changeName] = useState({
+        fName: "",
+        lName: ""
+    })
 
+    function inputChange(event) {
+        let value = event.target.value;
+        let name = event.target.name; // donot use the event object inside state change functions
+
+        changeInputValue((prevState) => {
+            //prev state hold the values as per the last change in state
+            let temp = {
+                fName: prevState.fName,
+                lName: prevState.lName,
+            };
+            //doing this in order to keep the state of the other object as previously
+            if (name === "fName") {
+                temp.fName = value;
+            } else if (name === "lName") {
+                temp.lName = value;
+            }
+            return temp;
+        });
+    }
     function formSubmit(event) {
         changeName(inputValue);
-        changeInputValue(""); //clean up the input for new input
+        changeInputValue({
+            fName: "",
+            lName: ""
+        }); //clean up the input for new input
         event.preventDefault(); // this is to prevent the next step on form submit which is page refresh
     }
 
     return (
         <div className="container">
-            <h1>Hello {name}</h1>
+            <h1>Hello {name.fName} {name.lName}</h1>
             <form onSubmit={formSubmit}>
-                <input type="text" placeholder="What's your name?" value={inputValue}
-                       onChange={event => changeInputValue(event.target.value)}/>
+                <input type="text" name="fName" placeholder="First Name" value={inputValue.fName}
+                       onChange={inputChange}/>
+                <input type="text" name="lName" placeholder="Last Name" value={inputValue.lName}
+                       onChange={inputChange}/>
                 <button type="submit" style={{backgroundColor: isMouseOver ? "black" : "white"}}
                         onMouseOver={()=>changeState(true)}
                         onMouseOut={()=>changeState(false)}>
