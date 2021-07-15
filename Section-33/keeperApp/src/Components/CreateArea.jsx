59aa9df3dddc3ec9831f00a96ee7f016f89e4917
somone-codes <someone_mails@protonmail.com>
2021-07-16 00:05:59 +0530

    Section-33 REACT KeeperApp - Adding Expanding feature

--- Section-33/keeperApp/src/Components/CreateArea.jsx
+++ Section-33/keeperApp/src/Components/CreateArea.jsx
@@ -6,25 +6,35 @@ import Zoom from "@material-ui/core/Zoom";
 function CreateArea(props) {
 
     let [note, noteState] = useState({title:"", content: ""});
+    let [expanded, expandedState] = useState(false);
 
     function formSubmit(event) {
         props.onSubmit(note);
         noteState({title:"", content: ""});
+        expandedState(false);
         event.preventDefault();
     }
 
     function onInput(event) {
+        expandedState(true);
         noteState(prevState => ( {...prevState, [event.target.name]:event.target.value} ) );
     }
 
     return (
         <div>
             <form className="create-note">
-                <input name="title" placeholder="Title" value={note.title} onChange={ onInput }/>
-                <textarea name="content" placeholder="Take a note..." rows="3" value={note.content} onChange={ onInput }/>
-                <Fab aria-label="add" onClick={formSubmit}>
-                    <AddIcon />
-                </Fab>
+                {
+                    expanded
+                    &&
+                    <input name="title" placeholder="Title" value={note.title} onChange={ onInput } hidden={!expanded} />
+                }
+                <textarea name="content" placeholder="Take a note..." rows={expanded ? 3: 1} value={note.content}
+                          onChange={ onInput }/>
+                <Zoom in={expanded}>
+                    <Fab aria-label="add" onClick={formSubmit}>
+                        <AddIcon />
+                    </Fab>
+                </Zoom>
             </form>
         </div>
     );
