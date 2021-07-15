f0484db13c22d60baef23796bd2d26029baa0e7a
somone-codes <someone_mails@protonmail.com>
2021-07-15 23:15:33 +0530

    Section-33 REACT KeeperApp with feature to add notes from UI

--- Section-33/keeperApp/src/Components/CreateArea.jsx
+++ Section-33/keeperApp/src/Components/CreateArea.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+
+    let [note, noteState] = useState({title:"", content: ""});
+
+    function formSubmit(event) {
+        props.onSubmit(note);
+        noteState({title:"", content: ""});
+        event.preventDefault();
+    }
+
+    function onInput(event) {
+        noteState(prevState => ( {...prevState, [event.target.name]:event.target.value} ) );
+    }
+
     return (
         <div>
-            <form>
-                <input name="title" placeholder="Title" />
-                <textarea name="content" placeholder="Take a note..." rows="3" />
-                <button>Add</button>
+            <form onSubmit={formSubmit}>
+                <input name="title" placeholder="Title" value={note.title} onChange={ onInput }/>
+                <textarea name="content" placeholder="Take a note..." rows="3" value={note.content} onChange={ onInput }/>
+                <button type="submit">Add</button>
             </form>
         </div>
     );
--- Section-33/keeperApp/src/Components/KeeperApp.jsx
+++ Section-33/keeperApp/src/Components/KeeperApp.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+    let [notes, notesState] = useState([])
+
+    function addNote(note) {
+        notesState(prevState => ([...prevState, {title: note.title, content: note.content}]) );
+    }
+
+    function deleteNote(noteId) {
+        notesState((prevState => {
+            return prevState.filter((value, index) => noteId !== index)
+        }))
+    }
+
+    function generateNote(key, title, content) {
+        return <Note key={key} id={key} title={title} content={content} deleteNote={deleteNote}/>;
+    }
+
     return (
         <div>
             <Header />
-            <CreateArea />
-            <Note key={1} title="Note title" content="Note content" />
+            <CreateArea onSubmit={addNote}/>
+            { notes.map((value, index) => generateNote(index, value.title, value.content)) }
             <Footer />
         </div>
     );
