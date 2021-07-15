0a21cbf83aa92d5675ef1c1e9ced18a1417b55c0
somone-codes <someone_mails@protonmail.com>
2021-07-15 09:57:04 +0530

    Section-33 REACT KeeperApp Refactor to support adding notes from UI (starter)

--- Section-33/keeperApp/src/Components/KeeperApp.jsx
+++ Section-33/keeperApp/src/Components/KeeperApp.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../starterNotes"
+import CreateArea from "./CreateArea";
 
-function KeeperApp() {
-    let test = true;
-    return  <div>
-                <Header/>
-                { test === true && <Note key="0" noteTitle="This is a test title" noteContent="This is a test content"/> }
-                { test === true ? undefined :
-                    notes.map((note)=> <Note key={note.id} noteTitle={note.title} noteContent={note.content}/>)}
-                <Footer/>
-            </div>
+function App() {
+    return (
+        <div>
+            <Header />
+            <CreateArea />
+            <Note key={1} title="Note title" content="Note content" />
+            <Footer />
+        </div>
+    );
 }
 
-export default KeeperApp
+export default App;
--- Section-33/keeperApp/src/Components/Note.jsx
+++ Section-33/keeperApp/src/Components/Note.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 function Note(props) {
-    return <div className="note">
-            <h1>{props.noteTitle }</h1>
-            <p>{props.noteContent}</p>
+    return (
+        <div className="note">
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
+            <button>DELETE</button>
         </div>
+    );
 }
 
-export default Note
+export default Note;
